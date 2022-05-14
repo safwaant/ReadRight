228792436b1041d3674e2234182dd49efb024ed8
Safwaan Taher <62581576+safwaant@users.noreply.github.com>
2022-05-14 00:41:18 -0700

    Add skeleton HTML and voice

--- speech.js
+++ speech.js
@@ -1,3 +1,26 @@
-let speech = window.webkitSpeechRecognition
+if("webkitSpeechRecognition" in window){
+    let speechRecognition = new webkitSpeechRecognition();
+    speechRecognition.continous = true;
+    speechRecognition.interimResults = true;
+    let textBox = $("#textBox");
+    let content = "";
 
+    console.log("Speech Recognition Created");
+
+    speechRecognition.onstart = () => {
+        instructions.text("Recording Voice");
+    }
+
+    let instructions = $("#instructions")
+
+    $("#go").click((event) => {
+        if(content.length){
+            content +="";
+        }
+        speechRecognition.start();
+    });
+
+} else {
+    console.log("Failed to load speech recognition");
+}
 
